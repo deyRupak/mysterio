9aec11663c78eff7211886f37eeb21a9a83e80d7
neopentane <kolisomesh27@gmail.com>
2020-03-08 10:56:00 +0530

    uploadPage

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pickle
 import os,shutil
 import pickle
 from PIL import Image
-
+import matplotlib
+import matplotlib.pyplot as plt
 # import useful libraries
 import numpy as np # linear algebra
-from matplotlib import pyplot as plt
 from PIL import Image
 from glob import glob
 import os
@@ -25,7 +25,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 # define paths
-train_path = './intel-image-classification/seg_train/seg_train'
+train_path = './intel-image-classification/seg_test/seg_test'
 test_path = './intel-image-classification/seg_test/seg_test'
 pred_path = 'static/upload'
 
@@ -37,18 +37,39 @@ transformer = transforms.Compose([
                          [0.5, 0.5, 0.5])
 ])
 
-train_loader = DataLoader(
-    ImageFolder(train_path, transform=transformer),
-    num_workers=8, batch_size=200, shuffle=True
-)
+
 model = torch.load("model/resnet.pkl")
 
 UPLOAD_FOLDER = 'uploads'
+train_loader = DataLoader(ImageFolder(train_path, transform=transformer),num_workers=8, batch_size=200, shuffle=True)
 
+error = nn.CrossEntropyLoss()
+opt = optim.Adam(model.parameters())
+a=[]
+b=[]
+def train(model, train_loader, n_epochs=100):
+	model = model.to(device)
+	model.train()
+	for epoch in range(n_epochs):
+		epoch_loss = 0
+		for x, y in train_loader:
+			x, y = x.to(device), y.to(device)
+			opt.zero_grad()
+			out = model(x)
+			loss = error(out, y)
+			loss.backward()
+			opt.step()
+			epoch_loss += loss.item()        
+		if epoch % int(0.1*n_epochs) == 0:
+			a.append(epoch)
+			b.append(epoch_loss)
+			fig,ax= plt.subplots()
+			ax.plot(a,b)
+			ax.set(xlabel="Training Loss",ylabel="Epoch")
+			fig.savefig("static/loss.png")
 
-
-
-
+			#print(a,b)
+			#print(f'epoch: {epoch} \t Train Loss: {epoch_loss:.4g}')
 classes = train_loader.dataset.class_to_idx
 
 def predict(model, path, sample_size):
@@ -64,8 +85,7 @@ def predict(model, path, sample_size):
 					ds.update({f.filename :key})
 					i+=1
 				else :
-					print(out)
-			plt.imshow(np.array(f))
+					pass
             
             
 	return ds
@@ -74,22 +94,7 @@ def predict(model, path, sample_size):
 error = nn.CrossEntropyLoss()
 opt = optim.Adam(model.parameters())
 
-def train(model, train_loader, n_epochs=100):
-    model = model.to(device)
-    model.train()
-    
-    for epoch in range(n_epochs):
-        epoch_loss = 0
-        for x, y in train_loader:
-            x, y = x.to(device), y.to(device)
-            opt.zero_grad()
-            out = model(x)
-            loss = error(out, y)
-            loss.backward()
-            opt.step()
-            epoch_loss += loss.item()        
-        if epoch % int(0.1*n_epochs) == 0:
-            print(f'epoch: {epoch} \t Train Loss: {epoch_loss:.4g}')
+
             
 app = Flask(__name__)
 app.config['UPLOAD_PATH'] = 'static/upload'
@@ -119,13 +124,14 @@ def home():
 
 @app.route("/test")
 def test():
-    return render_template("takeTest.html",name='')
 
+    return render_template("takeTest.html",name='')
 
 
      
 @app.route('/test', methods=['POST']) 
 def dec():
+	model = torch.load("model/resnet.pkl")
 	if request.form == {}:
 		shutil.rmtree("static/upload")
 		os.makedirs("static/upload")
@@ -139,13 +145,38 @@ def dec():
 	else:
 		projectpath = request.form
 		print(projectpath)
-		return render_template("takeTest.html", pred=projectpath)
+		return render_template("results.html", pred=projectpath)
 
 
 
 @app.route("/frames")
 def frame():
     return render_template("frames.html")
+
+
+@app.route('/result', methods=['POST'])
+def edit():
+	a=[]
+	b=[]
+	dest_path='static/edit'
+	x=request.form
+	for val in x:	
+		try:
+			shutil.move('./'+val,dest_path+'/'+x[val])
+		except:
+			pass
+	dest_path='static/edit'
+	train_path = dest_path
+	train_loader = DataLoader(
+    ImageFolder(train_path, transform=transformer),
+    num_workers=8, batch_size=200, shuffle=True
+	)
+	train(model,train_loader)
+	#fig,ax= plt.subplot()
+	#ax.plot(a,b)
+	#ax.set(xlabel="Loss",ylabel="Epoch")
+	#fig.savefig("testloss.png")
+	return render_template("results.html")
 
 
 
